909aa8fa99e200ea62735932121773ca7e528ecf
luka <antonic.luka88@gmail.com>
2017-07-04 10:09:40 +0200

    classifiers for setup.py

--- setup.py
+++ setup.py
@@ -11,4 +11,11 @@ setup(
     license = 'MIT',
     packages = ['csv2md'],
     zip_safe = True,
+    classifiers = [
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
